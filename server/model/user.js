9611bd0f80cc19100c7d408159fb39d109abc431
wangshudong <wsd312@163.com>
2017-09-15 16:55:43 +0800

    init

--- server/model/user.js
+++ server/model/user.js
@@ -54,34 +54,13 @@ Promise.promisifyAll(Goods.prototype);
 
 module.exports = Goods;
 
+
+
+
+
+
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
-var userSchema = new Schema({
-    "userId": String,
-    "userName" : String,
-    "userPwd": String,
-    "orderList" : Array,
-    "cartList" : [
-      {
-        "productId": String,
-        "productName" : String,
-        "salePrice": Number,
-        "productNum": String,
-        "productImage": String,
-        "productUrl": String,
-        "checked": String
-      }
-    ],
-    "addressList":[
-      {
-        "addressId": String,
-        "userName": String,
-        "streetName": String,
-        "postCode": Number,
-        "tel": Number,
-        "isDefault": Boolean
-      }
-    ]
-})
+
 
 module.exports = mongoose.model("user",userSchema);
