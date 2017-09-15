f9c3e4eabd6977207437fe9b8d312869d2bc0f12
wangshudong <wsd312@163.com>
2017-09-15 16:56:32 +0800

    init

--- server/model/user.js
+++ server/model/user.js
@@ -1,66 +1,66 @@
-'use strict';
+// 'use strict';
 
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
+// var mongoose = require('mongoose');
+// var Schema = mongoose.Schema;
 
-var GoodsSchema = new Schema({
-  "userId": String,
-  "userName" : String,
-  "userPwd": String,
-  "orderList" : Array,
-  "cartList" : [
-    {
-      "productId": String,
-      "productName" : String,
-      "salePrice": Number,
-      "productNum": String,
-      "productImage": String,
-      "productUrl": String,
-      "checked": String
-    }
-  ],
-  "addressList":[
-    {
-      "addressId": String,
-      "userName": String,
-      "streetName": String,
-      "postCode": Number,
-      "tel": Number,
-      "isDefault": Boolean
-    }
-  ]
-});
+// var GoodsSchema = new Schema({
+//   "userId": String,
+//   "userName" : String,
+//   "userPwd": String,
+//   "orderList" : Array,
+//   "cartList" : [
+//     {
+//       "productId": String,
+//       "productName" : String,
+//       "salePrice": Number,
+//       "productNum": String,
+//       "productImage": String,
+//       "productUrl": String,
+//       "checked": String
+//     }
+//   ],
+//   "addressList":[
+//     {
+//       "addressId": String,
+//       "userName": String,
+//       "streetName": String,
+//       "postCode": Number,
+//       "tel": Number,
+//       "isDefault": Boolean
+//     }
+//   ]
+// });
 
-GoodsSchema
-  .virtual('info')
-  .get(function() {
-    return {
-    	'_id': this._id,
-      'title': this.title,
-      'content': this.content,
-      'images': this.images,
-      'visit_count': this.visit_count,
-      'comment_count':this.comment_count,
-      'like_count':this.like_count,
-      'publish_time': this.publish_time
-    };
-  });
+// GoodsSchema
+//   .virtual('info')
+//   .get(function() {
+//     return {
+//     	'_id': this._id,
+//       'title': this.title,
+//       'content': this.content,
+//       'images': this.images,
+//       'visit_count': this.visit_count,
+//       'comment_count':this.comment_count,
+//       'like_count':this.like_count,
+//       'publish_time': this.publish_time
+//     };
+//   });
 
-var Goods = mongoose.model('Goods',GoodsSchema);
+// var Goods = mongoose.model('Goods',GoodsSchema);
 
-var Promise = require('bluebird');
-Promise.promisifyAll(Goods);
-Promise.promisifyAll(Goods.prototype);
+// var Promise = require('bluebird');
+// Promise.promisifyAll(Goods);
+// Promise.promisifyAll(Goods.prototype);
 
-module.exports = Goods;
-
-
+// module.exports = Goods;
 
 
 
 
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
 
 
-module.exports = mongoose.model("user",userSchema);
+// // var mongoose = require('mongoose');
+// // var Schema = mongoose.Schema;
+
+
+// // module.exports = mongoose.model("user",userSchema);
